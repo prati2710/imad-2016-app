d9780271d4b61b76c85c0b7d70bcf59bc50c41e0
prati2710 <pratishthachaurasia@gmail.com>
2016-09-30 21:49:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,7 +63,8 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function (req, res) {
     //articleName == articleOne
     //articles[articleName]= {} content object for article one  
-  res.send(createTemplate(articles[articleName]));
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
