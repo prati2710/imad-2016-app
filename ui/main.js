f6c96ebc60d9324aa2eee73f3db353df75619b8c
prati2710 <pratishthachaurasia@gmail.com>
2016-10-02 20:36:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,7 @@ console.log('Loaded!');
 //change the text of the main text
 var ele = document.getElementById('main-text');
 ele.innerHTML = 'Something Fancy'; 
+var img = document.getElementById('dino');
+img.onclick = function () {
+  img.style.marginLeft = '100px';  
+};
