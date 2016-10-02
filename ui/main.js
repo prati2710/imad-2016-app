b89096f5a9eaee9dbe95b0de0b1393de86391c94
prati2710 <pratishthachaurasia@gmail.com>
2016-10-02 20:31:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,4 +2,4 @@ console.log('Loaded!');
  
 //change the text of the main text
 var ele = document.getElementById('main-text');
-ele.innerHTML = 'Something Fancy';
+ele.innerHTML = 'Something Fancy'; 
