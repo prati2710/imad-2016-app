7c0153224f1ce76721ae258a81ca854c469060a4
prati2710 <pratishthachaurasia@gmail.com>
2016-10-02 21:21:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,14 @@ console.log('Loaded!');
 //change the text of the main text
 var ele = document.getElementById('main-text');
 ele.innerHTML = 'Something Fancy'; 
+
+//Image modicifcation
 var img = document.getElementById('dino');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function () {
-  img.style.marginLeft = '100px';  
+ var interval = setInterval(moveRight,100);  
 };
