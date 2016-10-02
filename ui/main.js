8b32b26a942707a06c5e091d0902b56f2c17db03
prati2710 <pratishthachaurasia@gmail.com>
2016-10-02 20:45:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,5 +5,5 @@ var ele = document.getElementById('main-text');
 ele.innerHTML = 'Something Fancy'; 
 var img = document.getElementById('dino');
 img.onclick = function () {
-  img.style.marginLeft = '200px';  
+  img.style.marginLeft = '100px';  
 };
