95812017f5664d9b3bb287e0cc67c379139e5917
prati2710 <pratishthachaurasia@gmail.com>
2016-10-03 21:12:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,20 @@ button.onclick = function () {
     request.send(null);
     
 };
+
+//Submit Name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = socument.getElementById('submit_btn');
+submit.onclick = function() {
+  // Make a request to a servee and send the name
+  
+  //Captue a list of names and render it as a list
+  var names = ['name1','name2','name3','name4'];
+  var list = '';
+  for (var i=0;i<names.length;i++) {
+      list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
