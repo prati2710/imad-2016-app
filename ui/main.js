440b6bcb9752ea443bc0ad68d5e8a50a7be5c7aa
prati2710 <pratishthachaurasia@gmail.com>
2016-11-26 19:43:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,8 +23,8 @@ button.onclick = function () {
 };
 
 //Submit Name
-//var nameInput = document.getElementById('name');
-//name = nameInput.value;
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
   // Make a request to a servee and send the name
